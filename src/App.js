34f2af8dc06a88ff104aa577b2f2a17890a6eab6
AZP7 <aungazpzawphyo@gmail.com>
2025-04-05 10:10:50 +0630

    host updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './NavBar/Navbar';
-import './App.css'
-
+import HomePage from './HomePage/HomePage';
+import Destination from './Destination/Destination';
+import Crew from './Crew/Crew';
+import Technology from './Technology/Technology';
+import './App.css';
 
 export default function App() {
   return (
     <>
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/crew" element={<Crew />} />
+        <Route path="/technology" element={<Technology />} />
+      </Routes>
     </>
-  )
+  );
 }
